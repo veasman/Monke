27c419996d9e2d588b5b41d35c0619631b8037b6
veasman <charlton.moren@gmail.com>
2021-09-20 21:05:13 -0500

    Add file input

--- src/main.c
+++ src/main.c
@@ -1,19 +1,47 @@
 #include <stdio.h>
+#include <string.h>
 #include "include/lexer.h"
 #include "include/parser.h"
+#include "include/visitor.h"
+#include "include/io.h"
+
+#define MAX_LIMIT 20
+
+void print_help() {
+    printf("Usage:\nmonke.out <file>\n:");
+    exit(1);
+}
 
 int main(int argc, char* argv[]) {
-    lexer_t* lexer = init_lexer(
-            "var name = \"foo bar\";\n"
-            "print(name);\n"
-    );
+    if (argc >= 2) {
+        for (int i = 1; i < argc; i++) {
+            int len = strlen(argv[i]);
+            char* file_ext = &argv[i][len-6];
 
-    parser_t* parser = init_parser(lexer);
-    AST_T* root = parser_parse(parser, parser->scope);
-
-    printf("%d\n", root->type);
-    printf("%d\n", root->compound_size);
-
+            if (strcmp(file_ext, ".monke") == 0) {
+                lexer_t* lexer = init_lexer(get_file_contents(argv[i]));
+                parser_t* parser = init_parser(lexer);
+                AST_T* root = parser_parse(parser, parser->scope);
+                visitor_t* visitor = init_visitor();
+                visitor_visit(visitor, root);
+            }
+            else {
+                print_help();
+            }
+        }
+    }
+    else {
+        char input[MAX_LIMIT];
+        while (1) {
+            printf("Welcome to the monke language\n\n- veasman\n");
+            fgets(input,MAX_LIMIT, stdin);
+            lexer_t* lexer = init_lexer(input);
+            parser_t* parser = init_parser(lexer);
+            AST_T* root = parser_parse(parser, parser->scope);
+            visitor_t* visitor = init_visitor();
+            visitor_visit(visitor, root);
+        }
+    }
     return 0;
 }
 
